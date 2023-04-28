66c98154fbc00e92fe3a62e7fb6838a03c6f96aa
mavalero010 <maicolvpolo@gmail.com>
2023-04-27 23:02:31 -0500

    final

--- assets/js/historico.js
+++ assets/js/historico.js
@@ -35,24 +35,20 @@ const createDiv = ({ classDiv, children }) => {
   });
   return icono
   }
-  const deleteH=(cl)=>{
+
+  const deleteH=({cl,idarticle})=>{
+
     const c = cl.split(" ")
-    const icono = document.createElement("i")
+    const icono = document.createElement("button")
+    icono.setAttribute("value",idarticle)
+    icono.setAttribute("id",create_UUID())
     c.forEach(element => {
       icono.classList.add(element)
     });
     return icono
     }
-    const deleteAllH=(cl)=>{
-      const c = cl.split(" ")
-      const icono = document.createElement("i")
-      c.forEach(element => {
-        icono.classList.add(element)
-      });
-      return icono
-      }
     
-  
+ 
 const addRecord = ({ id, monedaOrigen, monedaDestino, monto, resultado,fecha }) => {
     const fragment = document.createDocumentFragment();
     const article = document.createElement("article");
@@ -75,13 +71,14 @@ const addRecord = ({ id, monedaOrigen, monedaDestino, monto, resultado,fecha }) 
       classDiv: "fechaActual text-blue-500 w-1/4 text-center",
       children: [createP({ text: fecha })],
     });
-  
+    const deleteBtn=deleteH({cl:"delete-register fa-solid fa-x text-red-500",idarticle:id})
     //article.appendChild(divMonto);
     article.appendChild(divMonedaOrigen);
     article.appendChild(ico);
     //article.appendChild(divResultado);
     article.appendChild(divMonedaDestino);
     article.appendChild(divFechaActual);
+    article.appendChild(deleteBtn);
     fragment.appendChild(article);
     return fragment;
   };
@@ -101,5 +98,49 @@ export const historicos =(historicos)=>{
 historicos(JSON.parse(
     window.localStorage.getItem("taskStorage")
   ))
+  
 
-  //window.localStorage.removeItem("taskStorage")
+  
+
+  function create_UUID() {
+    var dt = new Date().getTime();
+    var uuid = "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(
+      /[xy]/g,
+      function (c) {
+        var r = (dt + Math.random() * 16) % 16 | 0;
+        dt = Math.floor(dt / 16);
+        return (c == "x" ? r : (r & 0x3) | 0x8).toString(16);
+      }
+    );
+    return uuid;
+  }
+
+  const btn_borrar_todo=document.querySelector(".borrar-todo")
+
+  btn_borrar_todo.addEventListener("click",(e)=>{
+    window.localStorage.removeItem("taskStorage")
+    location.replace("historico.html")
+  })
+
+/*  const botones = document.querySelectorAll('.delete-register');
+  let taskStorage=JSON.parse(window.localStorage.getItem("taskstorage"))
+  console.log("nre: ",(taskStorage))
+  for (let i = 0; i < botones.length; i++) {
+    botones[i].addEventListener('click', function(event) {
+      const id = event.target.id;
+      const bt = document.getElementById(id)
+      bt.addEventListener("click", function() {
+        const idAEliminar = this.value; // obtiene el valor de la propiedad "value" del botón
+        const elementoAEliminar = document.getElementById(idAEliminar);
+        if (elementoAEliminar) { // verifica si se encontró el elemento a eliminar
+          elementoAEliminar.remove(); 
+          const newStorage = taskStorage.filter(t=>t.id==idAEliminar)
+          console.log("nre: ",newStorage)
+          
+          location.replace("historico.html")
+          // elimina el elemento del DOM
+        }
+      });
+      // Realiza aquí la acción que desees con el botón presionado
+    });
+  }*/
